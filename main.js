a9f9a0f51626fdc9f14635221de9e1c160944534
Smithgodfrey <128751207+Smithgodfrey@users.noreply.github.com>
2026-05-10 23:34:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:3000/api' 
+    : 'https://backend-production-29285.up.railway.app/api';
 
   
 
